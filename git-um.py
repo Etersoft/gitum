88030d3cd5815faf947ae2c43a87e4b91fe60fb7
Pavel Shilovsky <piastry@etersoft.ru>
2011-10-11 19:29:58 +0400

    Code style fixes

--- git-um.py
+++ git-um.py
@@ -20,7 +20,8 @@
 
 from git import *
 from subprocess import Popen
-import os, sys
+import os
+import sys
 
 upstream_branch = 'upstream'
 rebased_branch = 'rebased'
@@ -36,38 +37,38 @@ CONFIG_FILE = '.git-um-config'
 
 class GitUpstream(object):
 	def __init__(self, repo_path='.'):
-		self.repo = Repo(repo_path)
-		self.state = START_ST
-		self.id = 0
-		self.commits = []
-		self.saved_branches = {}
+		self._repo = Repo(repo_path)
+		self._state = START_ST
+		self._id = 0
+		self._commits = []
+		self._saved_branches = {}
 		self._load_config(CONFIG_FILE)
 
 	def pull(self, server, branch):
-		self.repo.git.fetch(server)
-		self.commits = self._get_commits(server, branch)
-		self.commits.reverse()
+		self._repo.git.fetch(server)
+		self._commits = self._get_commits(server, branch)
+		self._commits.reverse()
 		self._save_branches()
 		self._process_commits()
 
 	def abort(self):
 		self._load_state()
 		try:
-			self.repo.git.rebase('--abort')
+			self._repo.git.rebase('--abort')
 		except:
 			pass
 		self._restore_branches()
 
 	def continue_pull(self):
 		self._load_state()
-		if self.state == REBASE_ST:
+		if self._state == REBASE_ST:
 			try:
-				diff_str = self._stage2(self.commits[self.id], True)
-				self._stage3(self.commits[self.id], diff_str)
-				self.id += 1
+				diff_str = self._stage2(self._commits[self._id], True)
+				self._stage3(self._commits[self._id], diff_str)
+				self._id += 1
 			except GitCommandError as e:
 				self._save_state()
-				print e.stdout
+				print(e.stdout)
 				return
 			except:
 				self._save_state()
@@ -100,31 +101,31 @@ class GitUpstream(object):
 		return 0
 
 	def _restore_branches(self):
-		git = self.repo.git
+		git = self._repo.git
 		git.checkout(upstream_branch, '-f')
-		git.reset(self.saved_branches[upstream_branch], '--hard')
+		git.reset(self._saved_branches[upstream_branch], '--hard')
 		git.checkout(rebased_branch, '-f')
-		git.reset(self.saved_branches[rebased_branch], '--hard')
+		git.reset(self._saved_branches[rebased_branch], '--hard')
 		git.checkout(current_branch, '-f')
-		git.reset(self.saved_branches[current_branch], '--hard')
+		git.reset(self._saved_branches[current_branch], '--hard')
 
 	def _save_branches(self):
-		git = self.repo.git
-		self.saved_branches[upstream_branch] = self.repo.commit(upstream_branch).id
-		self.saved_branches[rebased_branch] = self.repo.commit(rebased_branch).id
-		self.saved_branches[current_branch] = self.repo.commit(current_branch).id
+		git = self._repo.git
+		self._saved_branches[upstream_branch] = self._repo.commit(upstream_branch).id
+		self._saved_branches[rebased_branch] = self._repo.commit(rebased_branch).id
+		self._saved_branches[current_branch] = self._repo.commit(current_branch).id
 
 	def _get_commits(self, server, branch):
-		return [q.id for q in self.repo.log(upstream_branch + '..' + server + '/' + branch)]
+		return [q.id for q in self._repo.log(upstream_branch + '..' + server + '/' + branch)]
 
 	def _process_commits(self):
 		try:
-			for i in xrange(self.id, len(self.commits)):
-				self._process_commit(self.commits[i])
-				self.id += 1
+			for i in xrange(self._id, len(self._commits)):
+				self._process_commit(self._commits[i])
+				self._id += 1
 		except GitCommandError as e:
 			self._save_state()
-			print e.stdout
+			print(e.stdout)
 		except:
 			self._save_state()
 			raise
@@ -145,32 +146,32 @@ class GitUpstream(object):
 		return status
 
 	def _stage1(self, commit):
-		git = self.repo.git
-		self.state = MERGE_ST
+		git = self._repo.git
+		self._state = MERGE_ST
 		git.checkout(upstream_branch)
 		print('merge commit ' + commit)
 		git.merge(commit)
 
 	def _stage2(self, commit, continue_rebase=False):
-		git = self.repo.git
-		self.state = REBASE_ST
+		git = self._repo.git
+		self._state = REBASE_ST
 		if continue_rebase:
 			git.rebase('--continue')
 		else:
 			git.checkout(rebased_branch)
-			self.saved_branches['prev_head'] = self._repo.commit(rebased_branch).id
+			self._saved_branches['prev_head'] = self._repo.commit(rebased_branch).id
 			git.rebase(commit)
-		diff_str = self.repo.diff(self.saved_branches['prev_head'], rebased_branch)
+		diff_str = self._repo.diff(self._saved_branches['prev_head'], rebased_branch)
 		return diff_str
 
 	def _stage3(self, commit, diff_str):
-		git = self.repo.git
-		self.state = COMMIT_ST
+		git = self._repo.git
+		self._state = COMMIT_ST
 		git.checkout(current_branch)
 		if self._patch_tree(diff_str) != 0:
-			print 'error occurs during applying the patch'
+			print('error occurs during applying the commit %s' % commit)
 		git.add('-A')
-		mess = self._fix_commit_message(self.repo.commit(commit).message)
+		mess = self._fix_commit_message(self._repo.commit(commit).message)
 		git.commit('-m', mess)
 
 	def _fix_commit_message(self, mess):
@@ -182,23 +183,23 @@ class GitUpstream(object):
 
 	def _save_state(self):
 		with open(PULL_FILE, 'w') as f:
-			f.write(self.saved_branches[upstream_branch] + '\n')
-			f.write(self.saved_branches[rebased_branch] + '\n')
-			f.write(self.saved_branches[current_branch] + '\n')
-			f.write(self.saved_branches['prev_head'] + '\n')
-			f.write(str(self.state) + '\n')
-			for i in xrange(self.id, len(self.commits)):
-				f.write(str(self.commits[i]) + '\n')
+			f.write(self._saved_branches[upstream_branch] + '\n')
+			f.write(self._saved_branches[rebased_branch] + '\n')
+			f.write(self._saved_branches[current_branch] + '\n')
+			f.write(self._saved_branches['prev_head'] + '\n')
+			f.write(str(self._state) + '\n')
+			for i in xrange(self._id, len(self._commits)):
+				f.write(str(self._commits[i]) + '\n')
 
 	def _load_state(self):
 		with open(PULL_FILE, 'r') as f:
-			self.saved_branches[upstream_branch] = f.readline().split()[0]
-			self.saved_branches[rebased_branch] = f.readline().split()[0]
-			self.saved_branches[current_branch] = f.readline().split()[0]
-			self.saved_branches['prev_head'] = f.readline().split()[0]
-			self.state = int(f.readline())
+			self._saved_branches[upstream_branch] = f.readline().split()[0]
+			self._saved_branches[rebased_branch] = f.readline().split()[0]
+			self._saved_branches[current_branch] = f.readline().split()[0]
+			self._saved_branches['prev_head'] = f.readline().split()[0]
+			self._state = int(f.readline())
 			for i in f.readlines():
-				self.commits.append(i.split()[0])
+				self._commits.append(i.split()[0])
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
