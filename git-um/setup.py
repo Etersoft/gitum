95051f6f8711c0781264486c30a722421bc1658a
Pavel Shilovsky <piastry@altlinux.org>
2011-10-31 14:18:28 +0400

    Add setup.py

---
+++ git-um/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# git-um - Git Upstream Manager.
+# Copyright (C) 2011  Pavel Shilovsky <piastry@etersoft.ru>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from setuptools import setup
+
+setup(
+	name = "git-um",
+	version = "0.1",
+	license = "GPLv2",
+	author = "Pavel Shilovsky",
+	author_email = "piastry@etersoft.ru",
+	description = "Git Upstream Manager",
+	packages = ["gitupstream"],
+	scripts = ["git-um"]
+)
