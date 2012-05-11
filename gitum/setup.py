854e1c4d6fa93bcea8aa4808a26bf1cc060c2e6c
Pavel Shilovsky <piastry@etersoft.ru>
2012-05-11 11:23:27 +0400

    Change version in setup.py

--- gitum/setup.py
+++ gitum/setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 
 setup(
 	name = "gitum",
-	version = "0.5",
+	version = "0.6",
 	license = "GPLv2",
 	author = "Pavel Shilovsky",
 	author_email = "piastry@etersoft.ru",
